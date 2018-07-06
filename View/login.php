92b50b0abc709d5ce25bd10b20b2c581f26b5815
Matthew <matt381161@gmail.com>
2018-07-06 14:25:52 +0800

    FBSDK

--- View/login.php
+++ View/login.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
   <?php require('bootstrap.php'); ?>
+  <?php require('../Model/FBSDK.php'); ?>
   <title>Auth</title>
  </head>
  <body>
@@ -21,10 +22,47 @@
 		  <button type="submit" class="btn btn-default">Submit</button>
 		  <button class="btn btn-default" onClick="register()">Register</button>
 		</form>
+
+    <fb:login-button 
+      scope="public_profile,email"
+      onlogin="checkLoginState();">
+    </fb:login-button>
+    <div id="status">
+  </div>
  </body>
  <script type="text/javascript">
    function register(){
     document.location.href="/View/register.php";
    }
+
+   function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    if (response.status === 'connected') {
+      testAPI();
+    } else {
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    }
+  }
+
+   FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+
+   function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
+
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+    });
+  }
  </script>
 </html>
